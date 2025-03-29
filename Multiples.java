b21a3a4da27ba512c216730aec5c86acb72798fe
Papa <Sonnyp250@gmail.com>
2025-03-28 19:57:11 -0700

    Update Multiples.java

--- Multiples.java
+++ Multiples.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FindingMultiplesButThisTimeTakingUserInput {
+public class FindingMultiples {
         static Scanner irr = new Scanner(System.in);
 
     public static void main(String[] args) {
